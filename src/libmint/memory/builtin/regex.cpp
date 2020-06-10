39f184e60084efaedb40c53c8d38bc435cfdf473
Palamecia <palamecia@live.fr>
2020-06-10 18:53:23 +0200

    Adding methodes getFlags, match and search for regex

--- src/libmint/memory/builtin/regex.cpp
+++ src/libmint/memory/builtin/regex.cpp
@@ -3,6 +3,7 @@
 #include "memory/casttool.h"
 #include "ast/abstractsyntaxtree.h"
 #include "ast/cursor.h"
+#include "system/utf8iterator.h"
 
 using namespace mint;
 using namespace std;
@@ -14,6 +15,9 @@ RegexClass *RegexClass::instance() {
 }
 
 Regex::Regex() : Object(RegexClass::instance()) {}
+
+SharedReference match_to_iterator(const string &str, const smatch &match);
+SharedReference sub_match_to_iterator(const string &str, const smatch &match, size_t index);
 
 RegexClass::RegexClass() : Class("regex", Class::regex) {
 
@@ -58,4 +62,80 @@ RegexClass::RegexClass() : Class("regex", Class::regex) {
 							cursor->stack().pop_back();
 							cursor->stack().emplace_back(create_boolean(!regex_search(to_string(rvalue), self->data<Regex>()->expr)));
 						}));
+
+	createBuiltinMember("match", 2, AbstractSyntaxTree::createBuiltinMethode(metatype(), [] (Cursor *cursor) {
+
+							size_t base = get_stack_base(cursor);
+
+							SharedReference str = move(cursor->stack().at(base));
+							SharedReference self = move(cursor->stack().at(base - 1));
+
+							smatch match;
+							smatch::string_type s = to_string(str);
+
+							if (regex_match(s, match, self->data<Regex>()->expr)) {
+								cursor->stack().pop_back();
+								cursor->stack().pop_back();
+								cursor->stack().emplace_back(match_to_iterator(s, match));
+							}
+							else {
+								cursor->stack().pop_back();
+								cursor->stack().pop_back();
+								cursor->stack().emplace_back(SharedReference::unique(StrongReference::create<None>()));
+							}
+						}));
+
+	createBuiltinMember("search", 2, AbstractSyntaxTree::createBuiltinMethode(metatype(), [] (Cursor *cursor) {
+
+							size_t base = get_stack_base(cursor);
+
+							SharedReference str = move(cursor->stack().at(base));
+							SharedReference self = move(cursor->stack().at(base - 1));
+
+							smatch match;
+							smatch::string_type s = to_string(str);
+
+							if (regex_search(s, match, self->data<Regex>()->expr)) {
+								cursor->stack().pop_back();
+								cursor->stack().pop_back();
+								cursor->stack().emplace_back(match_to_iterator(s, match));
+							}
+							else {
+								cursor->stack().pop_back();
+								cursor->stack().pop_back();
+								cursor->stack().emplace_back(SharedReference::unique(StrongReference::create<None>()));
+							}
+						}));
+
+	createBuiltinMember("getFlags", 1, AbstractSyntaxTree::createBuiltinMethode(metatype(), [] (Cursor *cursor) {
+							SharedReference self = move(cursor->stack().back());
+							cursor->stack().back() = create_string(self->data<Regex>()->initializer.substr(self->data<Regex>()->initializer.rfind('/') + 1));
+						}));
 }
+
+
+SharedReference match_to_iterator(const std::string &str, const smatch &match) {
+
+	SharedReference result = SharedReference::unique(StrongReference::create<Iterator>());
+
+	for (size_t index = 0; index < match.size(); ++index) {
+		iterator_insert(result->data<Iterator>(), sub_match_to_iterator(str, match, index));
+	}
+
+	result->data<Iterator>()->construct();
+	return result;
+}
+
+SharedReference sub_match_to_iterator(const string &str, const smatch &match, size_t index) {
+
+	SharedReference item = SharedReference::unique(StrongReference::create<Iterator>());
+	std::string match_str = match[index].str();
+
+	iterator_insert(item->data<Iterator>(), create_string(match_str));
+	iterator_insert(item->data<Iterator>(), create_number(utf8_byte_index_to_pos(str, match.position(index))));
+	iterator_insert(item->data<Iterator>(), create_number(utf8length(match_str)));
+
+	item->data<Iterator>()->construct();
+	return item;
+}
+
